eea75a08f045f6479d4573d0a8fa35e8126ad670
cjs10lib <34670425+cjs10lib@users.noreply.github.com>
2018-10-16 05:15:21 +0000

    Generating & designing a reister page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'inbox', loadChildren: './inbox/inbox.module#InboxPageModule' },
-  { path: 'groups', loadChildren: './groups/groups.module#GroupsPageModule' },
-  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' }
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
